cab54bca12c7aae82db127b05032f1bedbed8d9e
Haiiikyu <thomastef@hotmail.fr>
2025-12-13 21:35:44 +0100

    Add games page

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,5 +1,5 @@
 // utils/supabase/server.ts
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export async function createClient() {
@@ -13,7 +13,8 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        // Ajout du typage explicite ici
+        setAll(cookiesToSet: { name: string; value: string; options?: CookieOptions }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
